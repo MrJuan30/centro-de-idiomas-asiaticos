f24cdfd37ce9e29824a4c48145188eac5bf13e80
Sebastian Cantor <105733738+MrJuan30@users.noreply.github.com>
2026-08-02 02:00:41 -0500

    feat: global styles and central image registry

---
+++ src/lib/images.ts
@@ -0,0 +1,69 @@
+import heroKyoto from "@/assets/img/hero-kyoto.webp";
+import pagodaFuji from "@/assets/img/pagoda-fuji.webp";
+import shibuyaNight from "@/assets/img/shibuya-night.webp";
+import fushimiInari from "@/assets/img/fushimi-inari.webp";
+import itsukushimaTorii from "@/assets/img/itsukushima-torii.webp";
+import mtFuji from "@/assets/img/mt-fuji.webp";
+import sakuraRiver from "@/assets/img/sakura-river.webp";
+import sakuraClose from "@/assets/img/sakura-close.webp";
+import osakaStreet from "@/assets/img/osaka-street.webp";
+import templeLanterns from "@/assets/img/temple-lanterns.webp";
+import tokyoAlley from "@/assets/img/tokyo-alley.webp";
+import studentsLaptop from "@/assets/img/students-laptop.webp";
+import studentsLibrary from "@/assets/img/students-library.webp";
+import classroom from "@/assets/img/classroom.webp";
+import openBooks from "@/assets/img/open-books.webp";
+import libraryHall from "@/assets/img/library-hall.webp";
+import readingRoom from "@/assets/img/reading-room.webp";
+import kidsEducation from "@/assets/img/kids-education.webp";
+import teacher from "@/assets/img/teacher.webp";
+import notebookPen from "@/assets/img/notebook-pen.webp";
+import graduation from "@/assets/img/graduation.webp";
+import onlineClass from "@/assets/img/online-class.webp";
+import businessMeeting from "@/assets/img/business-meeting.webp";
+import teamLaptops from "@/assets/img/team-laptops.webp";
+import matcha from "@/assets/img/matcha.webp";
+import sushiBoat from "@/assets/img/sushi-boat.webp";
+import sushiDark from "@/assets/img/sushi-dark.webp";
+import ramen from "@/assets/img/ramen.webp";
+import zenRoom from "@/assets/img/zen-room.webp";
+import portrait1 from "@/assets/img/portrait-1.webp";
+import portrait2 from "@/assets/img/portrait-2.webp";
+import portrait3 from "@/assets/img/portrait-3.webp";
+import portrait4 from "@/assets/img/portrait-4.webp";
+
+export const img = {
+  heroKyoto,
+  pagodaFuji,
+  shibuyaNight,
+  fushimiInari,
+  itsukushimaTorii,
+  mtFuji,
+  sakuraRiver,
+  sakuraClose,
+  osakaStreet,
+  templeLanterns,
+  tokyoAlley,
+  studentsLaptop,
+  studentsLibrary,
+  classroom,
+  openBooks,
+  libraryHall,
+  readingRoom,
+  kidsEducation,
+  teacher,
+  notebookPen,
+  graduation,
+  onlineClass,
+  businessMeeting,
+  teamLaptops,
+  matcha,
+  sushiBoat,
+  sushiDark,
+  ramen,
+  zenRoom,
+  portrait1,
+  portrait2,
+  portrait3,
+  portrait4,
+};
